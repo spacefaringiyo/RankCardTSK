f08da87aa35cb869e3e917b712f6d633eab189f3
spacefaringiyo <sliba908@gmail.com>
2026-04-20 09:09:40 +0900

    added horizon rank color palette

---
+++ rank-card-generator/src/layers/palettes/OrangePaletteV2.js
@@ -0,0 +1,13 @@
+export const OrangePaletteV2 = {
+    id: 'orange-v2',
+    name: 'UNKNOWN RANK (ORANGE V2)',
+    backgroundCanvas: "#000000",
+    backgroundGlowCenter: "#261700",
+    backgroundGlowEdge: "#0d0800",
+    primaryText: "#ff9900",
+    accentText: "#995c00",
+    outerBorderStroke: "#1a0f00",
+    innerBorderStroke: "#4d2e00",
+    logoText: "#ffb340",
+    motifAccent: "#ff8000",
+};
---
+++ rank-card-generator/src/layers/palettes/OrangePaletteV3.js
@@ -0,0 +1,13 @@
+export const OrangePaletteV3 = {
+    id: 'orange-v3',
+    name: 'HORIZON',
+    backgroundCanvas: "#000000",
+    backgroundGlowCenter: "#3d2500",
+    backgroundGlowEdge: "#1a0f00",
+    primaryText: "#ff9900",
+    accentText: "#e68a00",
+    outerBorderStroke: "#291800",
+    innerBorderStroke: "#754700",
+    logoText: "#ffad33",
+    motifAccent: "#e68a00",
+};
